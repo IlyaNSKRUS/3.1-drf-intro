6fb5cadd453f8766f95f0a332d0e15bdfc051185
Ilya NSK <Ilyagl.nsk@yandex.ru>
2024-09-09 22:16:37 +0700

    Edit Model

--- smart_home/measurement/models.py
+++ smart_home/measurement/models.py
@@ -6,7 +6,7 @@ class Sensor(models.Model):
     description = models.CharField(max_length=250)
 
 class Measurement(models.Model):
-    sensor = models.ForeignKey('Sensor', on_delete=models.CASCADE)
+    sensor = models.ForeignKey('Sensor', on_delete=models.CASCADE, related_name='measurements')
     temperature = models.FloatField()
     time_measurement = models.DateTimeField(auto_now_add=True)
     photo = models.ImageField(upload_to='photo/%y/%m/%d', blank=True)
